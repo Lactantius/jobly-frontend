4ba673a497741a8dd96c29eafbeeb65dfb02b1e3
Gerard Keiser <gerardkeiser@gmail.com>
2022-10-03 16:49:36 -0500

    Add login, register, logout, and user functions to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,46 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import jwt from "jsonwebtoken";
 
 import NavBar from "./NavBar";
 import { useLocalStorage } from "./hooks";
 import "./App.css";
 
 import Router from "./Router";
+import JoblyApi from "./api";
 
 function App(): JSX.Element {
-  const [token, getToken] = useLocalStorage("_token", "");
+  const [token, setToken] = useLocalStorage("token", "");
+
+  const login = async (username: string, password: string) => {
+    const authToken = await JoblyApi.login(username, password);
+    setToken(authToken);
+  };
+
+  const register = async (
+    username: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+    email: string
+  ) => {
+    const authToken = await JoblyApi.register(
+      username,
+      password,
+      firstName,
+      lastName,
+      email
+    );
+    setToken(authToken);
+  };
+
+  const logout = () => setToken({});
+
+  const user = (token) => jwt.decode(token);
 
   return (
     <BrowserRouter>
-      <NavBar />
+      <NavBar user={user} />
       <Router />
     </BrowserRouter>
   );
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar(): JSX.Element {
+interface NavBarProps {
+  user: object;
+}
+
+function NavBar({ user }: NavBarProps): JSX.Element {
   return (
     <nav className="NavBar">
       <NavLink to="/">Jobly</NavLink>
