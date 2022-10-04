c8d6f88d1b93817902879fabd3cf75498e4bf4c4
Gerard Keiser <gerardkeiser@gmail.com>
2022-10-03 20:47:10 -0500

    Add homepage logic for user/nonuser

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface HomeProps {
   user: UserToken | null;
 }
 
 function Home({ user }: HomeProps): JSX.Element {
-  return <div></div>;
+  if (user) {
+    return (
+      <div className="Home">
+        <p>Welcome back, {user.username}</p>
+      </div>
+    );
+  }
+  return (
+    <div className="Home">
+      <Link to="/login">Log in</Link>
+      <Link to="/signup">Sign up</Link>
+    </div>
+  );
 }
 
 export default Home;
